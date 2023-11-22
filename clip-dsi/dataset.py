c9466816f8f5ce33235ca211e3ef0d48d42aa79a
Neha <nehadesaraju@gmail.com>
2023-11-22 16:49:53 -0600

    start clipdsi implementation

--- clip-dsi/dataset.py
+++ clip-dsi/dataset.py
@@ -1,9 +1,10 @@
 import json
 from torch.utils.data import Dataset
 import os
+from PIL import Image
 
 class CLIPDSIDataset(Dataset):
-    def __init__(self, train_data_path, processor):
+    def __init__(self, train_data_path, processor, image_dir):
         super().__init__()
         self.processor = processor
         self.data = []
@@ -11,6 +12,7 @@ class CLIPDSIDataset(Dataset):
             for line in f:
                 self.data.append(json.loads(line))
         self.cache = [None for _ in range(len(self.data))]
+        self.image_dir = image_dir
 
         # the data is (text, text_id) pairs, where text could either be the
         # text of a document or a question, and text_id is the id of the text
@@ -22,11 +24,11 @@ class CLIPDSIDataset(Dataset):
         if self.cache[idx] != None:
             return self.cache[idx]
         else:
-            img_id, query_text, filename = self.data[idx].get("img_id"), 
-                    self.data[idx].get("query_text"), 
+            img_id, query_text, filename = self.data[idx].get("img_id"), \
+                    self.data[idx].get("query_text"), \
                     self.data[idx].get("img_filename")
 
-
+            # TODO: maybe tokenize using the decoder tokenizer instead of the encoder tokenizer
             id_encoding = self.processor(text=img_id,
                                     return_tensors="pt")
             labels = id_encoding.input_ids[0]
@@ -36,21 +38,27 @@ class CLIPDSIDataset(Dataset):
                 query_encoding = self.processor(text=query_text,
                                                 return_tensors="pt")
                 self.cache[idx] = {
-                    "input_ids": query_encoding.input_ids[0]
+                    "input_ids": query_encoding.input_ids[0],
                     "attention_mask": query_encoding.attention_mask[0],
+                    "position_ids": query_encoding.position_ids[0], 
+                    "image": torch.zeros((1, 3, 224, 224)) # create dummy image tensor,
                     "labels": labels,
-                    "target_id": str(img_id)
+                    "target_id": str(img_id),
+                    "image_example": False
                 }
 
             elif filename is not None:
-                # TODO: load image and call processor given image=
-                image = _
+                image = Image.open(os.path.join(self.image_dir, filename))
                 processed_image = self.processor(image=image,
                                                 return_tensors="pt")
-                # TODO check output of processor on image
                 self.cache[idx] = {
+                    "input_ids": torch.zeros((1, 1)), # create dummy input_ids tensor
+                    "attention_mask": torch.zeros((1, 1)), # create dummy attention_mask tensor
+                    "position_ids": torch.zeros((1, 1)), # create dummy position_ids tensor
                     "image": processed_image,
-
-                }                                    
+                    "labels": labels,
+                    "target_id": str(img_id),
+                    "image_example": True
+                }                             
         
             return self.cache[idx]
--- clip-dsi/model.py
+++ clip-dsi/model.py
@@ -1,32 +1,99 @@
 import os
 from lightning.pytorch.utilities.types import OptimizerLRScheduler
 from torch import optim, nn
-from transformers import CLIPProcessor, CLIPTextModel, CLIPVisionModel
+from transformers import CLIPProcessor, CLIPTextModel, CLIPVisionModel, T5ForConditionalGeneration, T5Tokenizer
 import lightning.pytorch as pl
 import torch
+from typing import Tuple, Optional
+
+
+class CLIPDSIDecoder(nn.Module):
+    def __init__(self, pretrained_language_model_name):
+        super().__init__()
+        self.decoder = T5ForConditionalGeneration.from_pretrained(pretrained_language_model_name)
+        self.tokenizer = T5Tokenizer.from_pretrained(pretrained_language_model_name)
+        self.start_input_id = [torch.tensor([self.tokenizer.bos_token_id])] # possibly only needed for beam search (inference)
+
+    def forward(
+        self, 
+        encoder_last_hidden_state: torch.Tensor,
+        encoder_hidden_states: Optional[torch.Tensor] = None, 
+        encoder_attentions: Optional[Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None
+    ):
+        return self.decoder(
+            input_ids=self.start_input_id,
+            attention_mask=attention_mask,
+            encoder_outputs=(encoder_last_hidden_state, encoder_hidden_states, encoder_attentions),
+            labels=labels
+        )
 
 
 class CLIPDSI(pl.LightningModule):
-    def __init__(self, model_name):
+    def __init__(self, encoder_model_name, decoder_model_name):
         super().__init__()
-        self.processor = CLIPProcessor.from_pretrained(model_name, cache_dir="cache")
-        self.text_model = CLIPTextModel.from_pretrained(model_name, cache_dir="cache")
-        self.vision_model = CLIPVisionModel.from_pretrained(model_name, cache_dir="cache")
+        self.processor = CLIPProcessor.from_pretrained(encoder_model_name, cache_dir="cache")
+        self.text_model = CLIPTextModel.from_pretrained(encoder_model_name, cache_dir="cache")
+        self.vision_model = CLIPVisionModel.from_pretrained(encoder_model_name, cache_dir="cache")
+        self.decoder = CLIPDSIDecoder(decoder_model_name)
+
+        # freeze encoder layers
+        for param in self.text_model.parameters():
+            param.requires_grad = False
+        for param in self.vision_model.parameters():
+            param.requires_grad = False
+
         self.val_step_outputs = []
 
-        # TODO: potentially freeze encoder layers of text_model and vision_model
 
-    def forward(self, input_ids=None, attention_mask=None, image=None):
+    def forward(self, input_ids=None, attention_mask=None, position_ids=None, image=None, labels=None):
+        with torch.no_grad():
+            # activate text encoder
+            if input_ids is not None:
+                output = self.text_model(input_ids=input_ids, attention_mask=attention_mask, position_ids=position_ids, return_dict=True)
 
-        # activate text tower
-        if input_ids is not None:
-            output = self.text_model(input_ids=input_ids, attention_mask=attention_mask, return_dict=True)
-            # compute embeddings?? (obtained from the projection on the text model)
-            # TODO: pass output (+ potential embeddings) to decoder layers (pooler_output or last_hidden_state) (the former most likely)
-            # 
+            # activate image encoder
+            elif image is not None:
+                output = self.image_model(pixel_values=image, return_dict=True)
+                
+            del output["pooler_output"]
 
-        # activate image tower
-        elif image is not None:
-            output = self.image_model(pixel_values=image, return_dict=True)
+        output = self.decoder(**output, labels=labels)
+        
+        return output
 
-            # TODO pass output to decoder layers (pooler_output or last_hidden_state)
+    def training_step(self, batch, batch_idx):
+        # filter so that correct batching can be applied to forward
+        im = batch["image"][image_example == True]
+        im_labels = batch["labels"][image_example == True]
+        
+        query_input_ids = batch["input_ids"][image_example == False]
+        query_attention_mask = batch["attention_mask"][image_example == False]
+        query_position_ids = batch["position_ids"][image_example == False]
+        query_labels = batch["labels"][image_example == False]
+
+        query_loss = 0
+        image_loss = 0
+
+        if query_input_ids:
+            query_loss = self.forward(
+                input_ids=query_input_ids,
+                attention_mask=query_attention_mask,
+                position_ids=query_position_ids,
+                labels=query_labels
+            ).loss
+        if im:
+            image_loss = self.forward(
+                image=im,
+                labels=im_labels
+            ).loss
+
+        loss = query_loss + image_loss
+
+        self.log('train_loss', loss, batch_size=batch['input_ids'].shape[0])
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        # TODO: implement beam search (normal inference) for validation
+        # pass
